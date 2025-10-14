b5fde40689e4b1e0fb8486b9703bbe365aee31c2
Cyber-701 <abdugofurovbehruzbek701@gmail.com>
2025-10-14 14:37:00 +0500

    Fixed view files

--- cinemadia/views.py
+++ cinemadia/views.py
@@ -25,7 +25,7 @@ def home(request):
     else:
         movies = Movie.objects.all().order_by('-created_at')
     
-    # Pagination
+    # Paginationn
     paginator = Paginator(movies, 12)  # 12 movies per page
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
